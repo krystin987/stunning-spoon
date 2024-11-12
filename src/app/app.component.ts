b7f946a8e8598db4abd48f57794add76f142c934
krystin987 <krystinv@gmail.com>
2024-11-12 12:36:23 -0700

    boilerplate, poetry component and service, works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PoetryComponent } from './poetry/poetry.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, PoetryComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-poetry></app-poetry>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/poetry/poetry.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PoetryService } from '../services/poetry.service';
+
+@Component({
+  selector: 'app-poetry',
+  standalone: true,
+  imports: [],
+  templateUrl: './poetry.component.html',
+  styleUrl: './poetry.component.css'
+})
+export class PoetryComponent {
+
+}
