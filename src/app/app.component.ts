8dee981290e66e88ab439925785ace942bb00b84
krystin987 <krystinv@gmail.com>
2024-11-16 21:00:01 -0700

    href stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PoetryComponent } from './poetry/poetry.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PoetryComponent],
+  imports: [RouterOutlet, PoetryComponent, NgOptimizedImage],
   template: `
     <main>
       <header class="app-header">
         <div class="header-container">
-          <img class="brand-logo" src="/assets/poem-poetry-icon.svg" alt="logo" aria-hidden="true" width="50px" height="50px"/>
+          <img class="brand-logo" src="/assets/poem-poetry-icon.svg" alt="logo" aria-hidden="true" width="50px"
+               height="50px"/>
           <div class="header-text">
             <h1 class="app-title">Poetry Viewer</h1>
             <p class="app-tagline">Discover and explore timeless poetry</p>
