7315111c187f237aa71f0da07e24240c7518da5e
krystin987 <krystinv@gmail.com>
2024-11-18 12:28:35 -0700

    Auto-generated commit

--- app/app-routing/app-routing.module.ts
+++ app/app-routing/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PoetryComponent } from '../components/poetry/poetry.component';
+import { PoetryComponent } from '../components/poetry-component/poetry.component';
 import { PoemDetailComponent } from '../components/poem-detail/poem-detail.component';
 
 const routes: Routes = [
