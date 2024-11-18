29dde738de1f01a428cb10aa139ae0d80fe0effa
krystin987 <krystinv@gmail.com>
2024-11-18 11:47:21 -0700

    Auto-generated commit

---
+++ app/components/poetry-list/poetry-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PoetryListComponent } from './poetry-list.component';
+
+describe('PoetryListComponent', () => {
+  let component: PoetryListComponent;
+  let fixture: ComponentFixture<PoetryListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PoetryListComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PoetryListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
